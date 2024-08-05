7f97ecaabdbcafa5f42d2a5728e0e3efe179023e
Aakash Sharma <aakash5427896@gmail.com>
2024-08-05 23:38:31 +0530

    commit

---
+++ SORTING/selection_sort.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"size of arr";
+    cin>>n;
+    int arr[n];
+    
+    cout<<"enter the number";
+    for(int i = 0;i < n;i++)
+    {
+        cin>>arr[i];
+    }
+      for(int i = 0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+   
+
+
+}
+
+ selection_sort(int arr[] , int size)
+    {
+           
+            for()
+    }
